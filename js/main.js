f87ffc4c2a6004ea7c5b98a0bd73914b6ae272da
ilya <65188527+ilyaip@users.noreply.github.com>
2020-10-07 20:39:07 +0300

    Personal project: more details

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var ARRAY_LENGHT = 25;
-var array = [];
-var COMMENT_ARRAY = [];
+var ARRAY_COMMENTS_LENGHT = 25;
+var randomComment = [];
+var COMMENTS = [];
 var COMMENT_NAME = ["Anton", "Max", "Kantimir", "Marusya", "Toma", "Joshua"];
 var COMMENT_DESCRIPTION = ["Всё отлично!", "В целом всё неплохо. Но не всё.", "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.", "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.", "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.", "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"];
 var pictureTemplate = document.querySelector("#picture").content.querySelector(".picture");
@@ -14,27 +14,27 @@ function getRandomInt(min, max) {
 
 var createComment = function (COMMENTS_NUMBER) {
   for (var i = 0; i < COMMENTS_NUMBER; i++) {
-    COMMENT_ARRAY[i] = {
+    COMMENTS.push({
       avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
       message: COMMENT_DESCRIPTION[getRandomInt(0, COMMENT_DESCRIPTION.length - 1)],
       name: COMMENT_NAME[getRandomInt(0, COMMENT_NAME.length - 1)]
-    }
+    });
   }
-}
+};
 
-var createArray = function () {
-  for (var i = 0; i < ARRAY_LENGHT; i++) {
+var createArrayComments = function () {
+  for (var i = 0; i < ARRAY_COMMENTS_LENGHT; i++) {
     var NUMBER_OF_COMMENT = getRandomInt(1, 5);
-    array[i] = {
+    randomComment.push({
       url: `photos/${getRandomInt(1, 25)}.jpg`,
       description: "Very nice pic",
       like: `${getRandomInt(15, 200)}`,
       comments: createComment(NUMBER_OF_COMMENT)
-    };
+    });
   }
 };
 
-createArray();
+createArrayComments();
 
 var getImg = function (className) {
   var picturElement = pictureTemplate.cloneNode(true);
@@ -54,7 +54,7 @@ var getComment = function (className) {
   picturElement.querySelector(className).textContent = array[i].comments;
 };
 
-for (var i = 0; i < array.length; i++) {
+for (var i = 0; i < randomComment.length; i++) {
   var picturElement = pictureTemplate.cloneNode(true);
 
   getImg(".picture__img");
